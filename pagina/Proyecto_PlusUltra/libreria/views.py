a35323ed281634a00d59bcfe27fffb01bb29ed32
ironbloody <50507370+ironbloody@users.noreply.github.com>
2022-06-12 22:28:46 -0400

    Add files via upload

--- pagina/Proyecto_PlusUltra/libreria/views.py
+++ pagina/Proyecto_PlusUltra/libreria/views.py
@@ -1,10 +1,55 @@
+from email import message
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from pyparsing import autoname_elements
+from django.contrib import messages
 
-from .models import Articulo
-from .forms import ArticuloForm
+from .models import Articulo, Tipo, Usuario
+from .forms import ArticuloForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django import template
+from django.contrib.auth.models import Group 
 
 # Create your views here.
+def Registro(request):
+        if request.user.is_authenticated:
+                return redirect('inicio')
+        else:
+                form = RegisterForm()
+                if request.method == 'POST':
+                        form = RegisterForm(request.POST)
+                        if form.is_valid():
+                                form.save()
+                                user = form.cleaned_data.get('username')
+                                messages.success(request, 'Account was created for ' + user)
+                                return redirect('login')
+			
+                context = {'form':form}
+        return render(request, 'paginas/registro.html', context)
+
+def Login(request):
+        if request.user.is_authenticated:
+                return redirect('inicio')
+        else:
+                if request.method == 'POST':
+                        username = request.POST.get('username')
+                        password =request.POST.get('password')
+
+                        user = authenticate(request, username=username, password=password)
+
+                        if user is not None:
+                                        login(request, user)
+                                        return redirect('inicio')
+                        else:
+                                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'paginas/login.html')
+def Logout(request):
+        logout(request)
+        messages.success(request, 'Yor Were logged out')
+        return redirect('inicio')
+
 def inicio(request):
         return render(request, 'paginas/inicio.html')
 
@@ -12,15 +57,14 @@ def home(request):
         return render(request, 'paginas/home.html')
 
 def libros(request): 
-        libros = Articulo.objects.all()
+        libros = Articulo.objects.filter(tipo = 1)
         return render(request, 'libros/libros.html', {'libros': libros})
-
 def mangas(request): 
-        mangas = Articulo.objects.all()
+        mangas = Articulo.objects.filter(tipo = 3)
         return render(request, 'libros/mangas.html', {'mangas': mangas})
 
 def comics(request): 
-        comics = Articulo.objects.all()
+        comics = Articulo.objects.filter(tipo = 4)
         return render(request, 'libros/comics.html', {'comics': comics})
 
 def crear_libro(request):
@@ -38,6 +82,14 @@ def crear_manga(request):
                 return redirect('mangas')
 
         return render(request, 'libros/crear_manga.html', {'formulario': formulario})
+
+def crear_comic(request):
+        formulario = ArticuloForm(request.POST or None, request.FILES or None)
+        if formulario.is_valid():
+                formulario.save()
+                return redirect('comics')
+
+        return render(request, 'libros/crear_comic.html', {'formulario': formulario})
         
 def editar(request, id):
         libro = Articulo.objects.get(id=id)
@@ -51,3 +103,11 @@ def eliminar(request, id):
         libro = Articulo.objects.get(id=id)
         libro.delete()
         return render('libro')
+
+
+register = template.Library() 
+
+@register.filter(name='has_group') 
+def has_group(user, group_name):
+    group =  Group.objects.get(name=group_name) 
+    return group in user.groups.all() 
